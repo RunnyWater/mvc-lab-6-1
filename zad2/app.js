ac3a27214633095d7b2cd74ef4367bf3a2ddac24
Vladyslav Krasynskyi <siriusbrightestone@gmail.com>
2024-04-23 08:36:19 +0200

    app

--- zad2/app.js
+++ zad2/app.js
@@ -7,7 +7,7 @@ app.set('views', path.join(__dirname, '/views'));
 
 
 
-app.use(express.static('public')); 
+app.use(express.static(path.join(__dirname, '/public'))); 
 app.set('view engine', 'ejs'); 
 app.use(session({
  secret: 'secret',
