14ace8021444270def88023ff5e738dc0a30195d
Vladyslav Krasynskyi <siriusbrightestone@gmail.com>
2024-04-23 08:30:16 +0200

    zad2

---
+++ zad2/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+app.set('views', path.join(__dirname, '/views'));
+
+
+
+app.use(express.static('public')); 
+app.set('view engine', 'ejs'); 
+app.use(session({
+ secret: 'secret',
+ resave: false,
+ saveUninitialized: true,
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:3000`);
+});
+
+const userRoutes = require('./routes/user');
+const bookRoutes = require('./routes/book');
+
+app.use('/user', userRoutes);
+app.use('/books', bookRoutes);
+
+
+
+const errorRoutes = require('./routes/error');
+app.use("*", errorRoutes);
